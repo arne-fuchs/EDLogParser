2ec34aa61894acf20eaee3e117d813745c7a6130
arne-fuchs <afuchsfuchs@aol.de>
2022-04-25 15:20:36 +0200

    Added basic icon.

--- src/main/java/de.paesserver/Main.java
+++ src/main/java/de.paesserver/Main.java
@@ -16,5 +16,6 @@ public class Main {
         MainFrame mainFrame = new MainFrame("EDLogParser");
         mainFrame.setSize(1420,900);
         mainFrame.setVisible(true);
+        mainFrame.setIconImage(new ImageIcon("org.edassets/materials/Basic.png").getImage());
     }
 }
